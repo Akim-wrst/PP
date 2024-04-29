2c68ba6a2324e2e4bff2621ec1710ddc69838bb4
Akim-wrst <156543468+Akim-wrst@users.noreply.github.com>
2024-04-29 20:15:52 +0300

    V24

--- src/test/java/ClientServiceImplTest.java
+++ src/test/java/ClientServiceImplTest.java
@@ -5,18 +5,18 @@ import com.example.pp.model.Client;
 import com.example.pp.model.Message;
 import com.example.pp.repository.ClientUniqueRepository;
 import com.example.pp.service.ClientServiceImpl;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.kafka.core.KafkaTemplate;
-
-import org.junit.jupiter.api.Test;
 import org.springframework.test.util.ReflectionTestUtils;
 
-import java.time.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Arrays;
-import java.util.List;
 
 import static org.mockito.Mockito.*;
 
@@ -38,18 +38,23 @@ public class ClientServiceImplTest {
     @InjectMocks
     private ClientServiceImpl clientService;
 
-    /*@Test
-    public void testSendUniqueClientMessagesBasedOnTime() {
+    @Test
+    void testSendUniqueClientMessagesBasedOnTime() {
         ReflectionTestUtils.setField(clientService, "currentTime", LocalTime.now());
+        ReflectionTestUtils.setField(clientService, "discount", "10%");
+        ReflectionTestUtils.setField(clientService, "topic_name", "test_topic");
+        ReflectionTestUtils.setField(clientService, "lastDigitOfNumber", "1");
+        ReflectionTestUtils.setField(clientService, "time", LocalTime.now());
 
         Client client1 = new Client("1", "Иван", "Иванов", "Иванович", 26L, LocalDate.now(), "89111234561");
         Client client2 = new Client("2", "Петр", "Петров", "Петрович", 24L, LocalDate.now(), "89117654321");
-        List<Client> clients = Arrays.asList(client1, client2);
-        when(clientFeignClient.getAllClients()).thenReturn(clients);
+        when(clientFeignClient.getAllClients()).thenReturn(Arrays.asList(client1, client2));
 
         ClientUnique model1 = new ClientUnique("Иванов Иван Иванович", "89111234563", LocalDate.now(), false);
         ClientUnique model2 = new ClientUnique("Петров Петр Петрович", "89117654322", LocalDate.now(), false);
+
         when(clientUniqueRepository.findAllClientsWhereMessageSendIsFalse()).thenReturn(Arrays.asList(model1, model2));
+        when(clientUniqueRepository.findPhoneByPhone(anyString())).thenReturn(null);
 
         Message smsMessage1 = new Message("89111234567", "Текст сообщения для Иван Иванов");
         when(clientUniqueMapper.clientToMessage(any(), any())).thenReturn(smsMessage1);
@@ -57,11 +62,17 @@ public class ClientServiceImplTest {
         clientService.sendUniqueClientMessagesBasedOnTime();
 
         verify(clientFeignClient, times(1)).getAllClients();
-        verify(kafkaTemplate, times(2)).send(any(), any());
-        verify(clientUniqueRepository, times(2)).updateClientMessageSendTrue(anyString());
-        verify(clientUniqueRepository, times(1)).findByPhone(anyString());
-        verify(clientUniqueRepository, times(1)).save(any());
-    }*/
+
+        ArgumentCaptor<String> topicCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<Message> messageCaptor = ArgumentCaptor.forClass(Message.class);
+        verify(kafkaTemplate, times(2)).send(topicCaptor.capture(), messageCaptor.capture());
+
+        ArgumentCaptor<String> phoneCaptor = ArgumentCaptor.forClass(String.class);
+        verify(clientUniqueRepository, times(2)).updateClientMessageSendTrue(phoneCaptor.capture());
+
+        ArgumentCaptor<ClientUnique> clientCaptor = ArgumentCaptor.forClass(ClientUnique.class);
+        verify(clientUniqueRepository, times(2)).save(clientCaptor.capture());
+    }
 
     @Test
     public void testProcessClientAndSendUniqueMessageIfApplicable() {
