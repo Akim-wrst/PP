960c588fcdac79982651aba7008d0c7d75b49eaa
Akim-wrst <156543468+Akim-wrst@users.noreply.github.com>
2024-05-12 18:21:55 +0300

    V34 kafka RETENTION_MS_CONFIG

--- src/main/java/com/example/pp/service/ClientServiceImpl.java
+++ src/main/java/com/example/pp/service/ClientServiceImpl.java
@@ -4,7 +4,6 @@ import com.example.pp.httpclient.ClientFeignClient;
 import com.example.pp.mapper.ClientUniqueMapper;
 import com.example.pp.model.Message;
 import com.example.pp.repository.ClientUniqueRepository;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -21,7 +20,6 @@ import java.util.Optional;
 
 @Slf4j
 @Service
-@Getter
 @RequiredArgsConstructor
 public class ClientServiceImpl implements ClientService {
     private final ClientUniqueRepository clientUniqueRepository;
@@ -48,21 +46,21 @@ public class ClientServiceImpl implements ClientService {
     @Override
     public void sendUniqueClientMessagesBasedOnTime() {
         try {
-            if (!currentTime.isAfter(getTime())) {
+            if (!currentTime.isAfter(time)) {
                 clientUniqueRepository.findAllClientsWhereMessageSendIsFalse()
                         .forEach(client -> {
-                            kafkaTemplate.send(getTopic_name(), clientUniqueMapper.clientToMessage(client, getDiscount()));
+                            kafkaTemplate.send(topic_name, clientUniqueMapper.clientToMessage(client, discount));
                             clientUniqueRepository.updateClientMessageSendTrue(client.getPhone());
                             log.info("Message sent for clients: {}", client.getPhone());
                         });
             } else {
-                log.info("Current time is past, {} no messages will be sent.", getTime());
+                log.info("Current time is past, {} no messages will be sent.", time);
             }
 
             clientFeignClient.getAllClients()
                     .parallelStream()
                     .filter(Objects::nonNull)
-                    .filter(client -> client.getPhone().endsWith(getLastDigitOfNumber())
+                    .filter(client -> client.getPhone().endsWith(lastDigitOfNumber)
                             && client.getBirthday().getMonth() == currentMonth)
                     .filter(client -> clientUniqueRepository.findPhoneByPhone(client.getPhone()) == null)
                     .forEach(client -> clientUniqueRepository.save(clientUniqueMapper.toUniqueClient(client)));
@@ -77,12 +75,12 @@ public class ClientServiceImpl implements ClientService {
     public void processClientAndSendUniqueMessageIfApplicable(String id) {
         try {
             Optional.ofNullable(clientFeignClient.getClient(id))
-                    .filter(client -> client.getPhone().endsWith(getLastDigitOfNumber())
+                    .filter(client -> client.getPhone().endsWith(lastDigitOfNumber)
                             && client.getBirthday().getMonth() == currentMonth)
                     .map(clientUniqueMapper::toUniqueClient)
                     .ifPresent(clientUnique -> {
-                        if (!currentTime.isAfter(getTime())) {
-                            kafkaTemplate.send(getTopic_name(), clientUniqueMapper.clientToMessage(clientUnique, getDiscount()));
+                        if (!currentTime.isAfter(time)) {
+                            kafkaTemplate.send(topic_name, clientUniqueMapper.clientToMessage(clientUnique, discount));
                             clientUnique.setMessageSend(true);
                             log.info("Message sent for client: {}", clientUnique.getPhone());
                         }
